ee5da4ba87d832e386e8b163f8b4c20ae4b49d50
Ilan Segal <ilan.segal@mail.utoronto.ca>
2022-12-18 11:33:59 -0500

    Parse raw input

--- 08-tree-house/main.py
+++ 08-tree-house/main.py
@@ -7,5 +7,10 @@ def get_raw_input() -> str:
     return open('08-tree-house/input.txt', 'r').read().strip().replace('\r\n', '\n')
 
 
+def get_tree_grid(raw_input: str) -> list[list[int]]:
+    parse_row = lambda row: list(map(int, row))
+    return list(map(parse_row, raw_input.split('\n')))
+
+
 if __name__ == '__main__':
     pass
