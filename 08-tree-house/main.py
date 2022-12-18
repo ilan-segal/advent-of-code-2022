67d8c2f050620599aad49bb781e745c69bb43986
Ilan Segal <ilan.segal@mail.utoronto.ca>
2022-12-18 11:32:38 -0500

    Initial commit to branch

---
+++ 08-tree-house/main.py
@@ -0,0 +1,11 @@
+"""
+https://adventofcode.com/2022/day/8
+"""
+
+
+def get_raw_input() -> str:
+    return open('08-tree-house/input.txt', 'r').read().strip().replace('\r\n', '\n')
+
+
+if __name__ == '__main__':
+    pass
