03cb2d64d3314a2a255a6e67972bbff1b1e991c7
Ilan-Voyager <ilan.segal@mail.utoronto.ca>
2022-12-31 15:46:00 -0500

    Begin work on day 15 part 2

--- 15-beacon/main.py
+++ 15-beacon/main.py
@@ -58,6 +58,49 @@ def part_1(raw_input: str) -> None:
     print(f'part_1={exclusion_size}')
 
 
+def manhattan_distance(p1: Coords, p2: Coords) -> int:
+    return sum(map(abs, [a - b for a, b in zip(p1, p2)]))  # type: ignore
+
+
+class Sensor:
+    pos: Coords
+    beacon_pos: Coords
+    __distance_to_beacon: int
+
+    def __init__(self, pos: Coords, beacon_pos: Coords) -> None:
+        self.pos = pos
+        self.beacon_pos = beacon_pos
+        self.__distance_to_beacon = manhattan_distance(self.pos, self.beacon_pos)
+
+    def exclusion_zone_contains(self, position: Coords) -> bool:
+        return manhattan_distance(self.pos, position) <= self.__distance_to_beacon
+
+    def get_exclusion_zone_boundary(self) -> list[Coords]:
+        """
+        Return list of all points which reside just outside of this Sensor's
+        exclusion zone.
+        """
+        boundary_side_length = self.__distance_to_beacon + 1
+        cur_pos = (self.pos[0], self.pos[1] - boundary_side_length)
+        boundary_points = [cur_pos]
+        for _ in range(boundary_side_length - 1):
+            cur_pos = (cur_pos[0] + 1, cur_pos[1] + 1)
+            boundary_points.append(cur_pos)
+        for _ in range(boundary_side_length - 1):
+            cur_pos = (cur_pos[0] - 1, cur_pos[1] + 1)
+            boundary_points.append(cur_pos)
+        for _ in range(boundary_side_length - 1):
+            cur_pos = (cur_pos[0] - 1, cur_pos[1] - 1)
+            boundary_points.append(cur_pos)
+        for _ in range(boundary_side_length - 1):
+            cur_pos = (cur_pos[0] + 1, cur_pos[1] - 1)
+            boundary_points.append(cur_pos)
+        return boundary_points
+
+    def drop_excluded_points(self, positions: list[Coords]) -> list[Coords]:
+        return [pos for pos in positions if not self.exclusion_zone_contains(pos)]
+
+
 if __name__ == '__main__':
     raw_input = get_raw_input()
     part_1(raw_input)
