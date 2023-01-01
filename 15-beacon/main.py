09a3dea4dd80b3626671caddc75c2ea7992a1a47
Ilan Segal <ilan.segal@mail.utoronto.ca>
2023-01-01 13:09:09 -0500

    Complete Day 15

--- 15-beacon/main.py
+++ 15-beacon/main.py
@@ -3,10 +3,24 @@ https://adventofcode.com/2022/day/15
 """
 
 
+from functools import reduce
 import re
+import time
+from typing import TypeVar, ParamSpec, Callable
 
 
 Coords = tuple[int, int]
+
+T = TypeVar('T')
+P = ParamSpec('P')
+def time_execution(f: Callable[P, T]) -> Callable[P, T]:
+    def wrapped(*args: P.args, **kwargs: P.kwargs) -> T:
+        start = time.time()
+        result = f(*args, **kwargs)
+        end = time.time()
+        print(f'{f.__name__} executed in {end-start:.5} s')
+        return result
+    return wrapped
 
 
 def get_raw_input() -> str:
@@ -50,6 +64,7 @@ def get_exclusion_size_at_y(y: int, sensor_beacon_map: dict[Coords, Coords]) -> 
     return sum(mask)
 
 
+@time_execution
 def part_1(raw_input: str) -> None:
     sensor_beacon_map = get_sensor_beacon_map(raw_input)
     row = 2_000_000
@@ -62,45 +77,71 @@ def manhattan_distance(p1: Coords, p2: Coords) -> int:
     return sum(map(abs, [a - b for a, b in zip(p1, p2)]))  # type: ignore
 
 
-class Sensor:
-    pos: Coords
+class SensorRegion:
+    sensor_pos: Coords
     beacon_pos: Coords
     __distance_to_beacon: int
 
     def __init__(self, pos: Coords, beacon_pos: Coords) -> None:
-        self.pos = pos
+        self.sensor_pos = pos
         self.beacon_pos = beacon_pos
-        self.__distance_to_beacon = manhattan_distance(self.pos, self.beacon_pos)
+        self.__distance_to_beacon = manhattan_distance(self.sensor_pos, self.beacon_pos)
 
-    def exclusion_zone_contains(self, position: Coords) -> bool:
-        return manhattan_distance(self.pos, position) <= self.__distance_to_beacon
+    def __contains__(self, position: Coords) -> bool:
+        return manhattan_distance(self.sensor_pos, position) <= self.__distance_to_beacon
 
-    def get_exclusion_zone_boundary(self) -> list[Coords]:
+    def get_boundary(self, range_min: int, range_max: int) -> set[Coords]:
         """
-        Return list of all points which reside just outside of this Sensor's
-        exclusion zone.
+        Return list of all points which reside just outside of this regin
         """
         boundary_side_length = self.__distance_to_beacon + 1
-        cur_pos = (self.pos[0], self.pos[1] - boundary_side_length)
-        boundary_points = [cur_pos]
+        cur_pos = (self.sensor_pos[0], self.sensor_pos[1] - boundary_side_length)
+        boundary_points = {cur_pos}
         for _ in range(boundary_side_length - 1):
             cur_pos = (cur_pos[0] + 1, cur_pos[1] + 1)
-            boundary_points.append(cur_pos)
+            if all(range_min <= p <= range_max for p in cur_pos):
+                boundary_points.add(cur_pos)
         for _ in range(boundary_side_length - 1):
             cur_pos = (cur_pos[0] - 1, cur_pos[1] + 1)
-            boundary_points.append(cur_pos)
+            if all(range_min <= p <= range_max for p in cur_pos):
+                boundary_points.add(cur_pos)
         for _ in range(boundary_side_length - 1):
             cur_pos = (cur_pos[0] - 1, cur_pos[1] - 1)
-            boundary_points.append(cur_pos)
+            if all(range_min <= p <= range_max for p in cur_pos):
+                boundary_points.add(cur_pos)
         for _ in range(boundary_side_length - 1):
             cur_pos = (cur_pos[0] + 1, cur_pos[1] - 1)
-            boundary_points.append(cur_pos)
+            if all(range_min <= p <= range_max for p in cur_pos):
+                boundary_points.add(cur_pos)
         return boundary_points
 
     def drop_excluded_points(self, positions: list[Coords]) -> list[Coords]:
-        return [pos for pos in positions if not self.exclusion_zone_contains(pos)]
+        return [pos for pos in positions if pos not in self]
+
+
+def get_sensor_regions_from_map(sensor_beacon_map: dict[Coords, Coords]) -> list[SensorRegion]:
+    return [SensorRegion(sensor_pos, beacon_pos) for sensor_pos, beacon_pos in sensor_beacon_map.items()]
+
+
+@time_execution
+def part_2(sensor_regions: list[SensorRegion]) -> None:
+    print('Getting boundaries')
+    boundaries = [region.get_boundary(0, 4_000_000) for region in sensor_regions]
+    print(f'Got {len(boundaries)} boundaries. Reducing to one set of points.')
+    boundary_points = reduce(set.union, boundaries)
+    def is_excluded(point: Coords) -> bool:
+        return any(point in region for region in sensor_regions)
+    print(f'{len(boundary_points)} points. Filtering by exclusion.')
+    for point in boundary_points:
+        if not is_excluded(point):
+            print(f'{point=}')
+            x, y = point
+            signal = 4_000_000 * x + y
+            print(f'part_2={signal}')
+            return
 
 
 if __name__ == '__main__':
     raw_input = get_raw_input()
     part_1(raw_input)
+    part_2(get_sensor_regions_from_map(get_sensor_beacon_map(raw_input)))
