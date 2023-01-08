51e0fae04aca3f7c243130662c7c8095d6b70a23
Ilan Segal <ilan.segal@mail.utoronto.ca>
2023-01-08 11:28:06 -0500

    Intermediary changes

--- 16-proboscidea/main.py
+++ 16-proboscidea/main.py
@@ -30,16 +30,29 @@ Thus our integer programming problem can be formulated as:
 Optimize:
     z = - (r_0 * t_0 + r_1 * t_1 + ...)
 Subject to:
-    t_i >= 1
-    -t_i >= -30
+    30 >= t_i >= 1
     ...
     t_i - t_j + 60 * B_ij >= 1 + d_ij           # https://lpsolve.sourceforge.net/5.1/absolute.htm
     t_j - t_i + 60 * (1 - B_ij) >= 1 + d_ij     # The original constraint:
-    B_ij >= 0                                   # | t_i - t_j | >= 1 + d_ij
-    -B_ij >= -1
+    1 >= B_ij >= 0                              # | t_i - t_j | >= 1 + d_ij
     ...
 
-If there are N optimizable valves then there are 2N + 4N^2 - 4N = 4N^2 - 2N constraints.
+How many constraints and variables are we optimizing?
+
+    Let there be N optimizable valves (valves with flow greater than 0).
+    Each such valve v_i entails the following constraints:
+        1 <= t_i <= 30
+    This adds N constraints and N decision variables.
+    There are also the following constraints for each i, j:
+        1 + d_ij <= t_i - t_j + 60 * B_ij
+        -59 + d_ij <= t_j - t_i - 60 * B_ij
+        0 <= B_ij <= 1
+    We do not include the above constraints for j, i or i, i because that would be redundant.
+    Thus this adds N*(N-1)*3/2 constraints and N*(N-1)/2 decision variables.
+
+    Thus we have:
+        N + N*(N-1)/2 decision variables (O(N^2))
+        N + N*(N-1)*3/2 constraints (O(N^2))
 """
 
 import heapq
@@ -104,12 +117,63 @@ def create_volcano_state(raw_input: str) -> tuple[DistanceMap, dict[str, int]]:
     return distances, optimizable_flows_by_label
 
 
+from scipy.optimize import (
+    Bounds,
+    LinearConstraint,
+    milp,
+    OptimizeResult,
+)
+import numpy as np
+
+
+def optimize_valve_times(distance_map: DistanceMap, valve_rate_by_label: dict[str, int]) -> OptimizeResult:
+    N = len(valve_rate_by_label)
+    num_B = (N * (N - 1)) // 2
+    num_variables = N + num_B
+    decision_bounds = Bounds(
+        np.array([1] * N + [0] * num_B),  # type: ignore
+        np.array([30] * N + [1] * num_B),  # type: ignore
+    )
+    constraints: list[LinearConstraint] = []
+    seen_pairs: set[tuple[str, str]] = set()
+    labels = list(valve_rate_by_label.keys())
+    for ai, a in enumerate(labels):
+        for bi, b in enumerate(labels):
+            if a == b:
+                continue
+            if (a, b) in seen_pairs or (b, a) in seen_pairs:
+                continue
+            seen_pairs.add((a, b))
+            distance = distance_map[a, b]
+            # Constraint: 1 + d_ij <= t_i - t_j + 60 * B_ij
+            A = np.zeros((1, num_variables))
+            A[0][ai] = 1
+            A[0][bi] = -1
+            A[0][N + len(seen_pairs) - 1] = 60
+            constraints.append(LinearConstraint(A, lb=1+distance))
+            # Constraint: -59 + d_ij <= t_j - t_i - 60 * B_ij
+            A = np.zeros((1, num_variables))
+            A[0][bi] = 1
+            A[0][ai] = -1
+            A[0][N + len(seen_pairs) - 1] = -60
+            constraints.append(LinearConstraint(A, lb=-59+distance))
+    return milp(
+        c=[valve_rate_by_label[label] for label in labels],
+        integrality=1,
+        bounds=decision_bounds,
+        constraints=constraints,
+        options={'disp': True},
+    )
+
+
 @time_execution
 def part_1(raw_input: str) -> None:
     distances, optimizable_flows_by_label = create_volcano_state(raw_input)
     print(distances)
     print(optimizable_flows_by_label)
-        
+    result = optimize_valve_times(distances, optimizable_flows_by_label)
+    print(result)
+
 
 if __name__ == '__main__':
     raw_input = get_raw_input()
