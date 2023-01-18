5e2eb1cbda7eb048e4841971827a975f0c75997d
Ilan-Voyager <ilan.segal@mail.utoronto.ca>
2023-01-17 23:43:34 -0500

    Complete Day 16

--- 16-proboscidea/main.py
+++ 16-proboscidea/main.py
@@ -6,8 +6,7 @@ from functools import cache
 import heapq
 import re
 import time
-from typing import TypeVar, ParamSpec, Callable
-from tqdm import tqdm
+from typing import TypeVar, ParamSpec, Callable, Iterable
 
 
 T = TypeVar('T')
@@ -62,8 +61,6 @@ def create_volcano_state(raw_input: str) -> tuple[PathMap, dict[str, int]]:
     distances: PathMap = dict()
     for start in valve_labels:
         for end in valve_labels:
-            if start == end:
-                continue
             distances[start, end] = resolve_path(start, end, adjacency_map)
     return distances, optimizable_flows_by_label
 
@@ -74,16 +71,76 @@ def my_print(*values: object) -> None:
     if not QUIET:
         print(*values)
 
+MEMORY: dict[tuple[tuple[str, ...], tuple[str, ...], int], int] = dict()
+MEMORY_ZERO: set[tuple[tuple[str, ...], tuple[str, ...], int]] = set()
+def get_pressure_released(
+    starting_spaces: tuple[str, ...],
+    remaining_valves: tuple[str, ...],
+    time_remaining: int) -> int:
+
+    key = (starting_spaces, remaining_valves, time_remaining)
+    if key in MEMORY_ZERO:
+        return 0
+    if key in MEMORY:
+        return MEMORY[key]
+
+    num_agents = len(starting_spaces)
+    num_valves = len(remaining_valves)
+    if time_remaining <= 0 or num_agents == 0 or num_valves == 0:
+        return 0
+
+    extra_agents = num_agents - num_valves
+    if extra_agents > 0:
+        # We have more agents than valves, so some agents are gonna nap for their next step
+        next_valves: tuple[str | None, ...] = remaining_valves + (None,) * extra_agents
+    else:
+        next_valves: tuple[str | None, ...] = remaining_valves
+
+    outcomes: list[int] = [0]
+    destination_permutations = get_non_repeating_product(next_valves, num_agents)
+    for destinations in destination_permutations:
+        starts_ends = zip(starting_spaces, destinations)
+        paths: list[list[str] | None] = list(map(path_map.get, starts_ends))  # type: ignore
+        real_paths = [path for path in paths if path is not None]
+        if len(real_paths) == 0:
+            continue
+        time_to_open_next_valve = min(map(len, real_paths))
+        time_remaining_after_valve_open = time_remaining - time_to_open_next_valve
+        pressure_released = 0
+        next_starting_spaces: list[str] = []
+        next_remaining_valves = set(remaining_valves)
+        for path in real_paths:
+            if len(path) != time_to_open_next_valve:
+                next_starting_spaces.append(path[time_to_open_next_valve])
+            else:
+                opened_valve_label = path[-1]
+                next_remaining_valves.remove(opened_valve_label)
+                next_starting_spaces.append(opened_valve_label)
+                rate = optimizable_flows_by_label[opened_valve_label]
+                pressure_released += rate * time_remaining_after_valve_open
+        outcome = pressure_released + get_pressure_released(
+            tuple(next_starting_spaces), 
+            tuple(next_remaining_valves), 
+            time_remaining_after_valve_open
+        )
+        outcomes.append(outcome)
+    ret = max(outcomes)
+    if ret == 0:
+        MEMORY_ZERO.add(key)
+    else:
+        MEMORY[key] = ret
+    return ret
+
 
 @cache
-def get_non_repeating_product(l: tuple[T, ...], n: int) -> tuple[tuple[T, ...]]:
+def get_non_repeating_product(l: Iterable[T], n: int) -> list[tuple[T, ...]]:
     """
     Returns l^n excluding tuples with repeated elements.
     """
     if n == 0:
-        return tuple()
+        return []
     if n == 1:
-        return tuple((el,) for el in l)
+        return [(el,) for el in l]
     sub_results = get_non_repeating_product(l, n-1)
     result: list[tuple[T, ...]] = []
     for sub_result in sub_results:
@@ -91,127 +148,18 @@ def get_non_repeating_product(l: tuple[T, ...], n: int) -> tuple[tuple[T, ...]]:
             if el in sub_result and el is not None:
                 continue
             result.append(sub_result + (el,))
-    return tuple(result)
-
-
-@cache
-def get_distance(start: str | None, end: str | None) -> int | float:
-    if start is None or end is None:
-        return float('inf')
-    if (start, end) not in path_map:
-        return float('inf')
-    return len(path_map[(start, end)]) - 1
-
-
-@cache
-def get_minimum_distance(starts: list[str | None], ends: list[str | None]) -> int | float:
-    zipped = zip(starts, ends)
-    distance = lambda t: get_distance(t[0], t[1])
-    return min(map(distance, zipped))
-
-
-cached_results: dict[tuple[str,...], int] = dict()
-
-
-def get_pressure_released(
-    agent_current_valves: tuple[str | None, ...],
-    remaining_rates: dict[str, int],
-    remaining_time: int) -> int:
-
-    key = (
-        tuple(sorted(map(str, agent_current_valves))), 
-        tuple(remaining_rates.keys()),
-        remaining_time,
-    )
-    if key in cached_results:
-        # print(f'Cache hit: {key} -> {cached_results[key]}')
-        return cached_results[key]
-
-    my_print('#'*10)
-    my_print(f'{agent_current_valves=}')
-    my_print(f'{remaining_rates=}')
-    my_print(f'{remaining_time=}')
-
-    if remaining_time <= 1 or len(remaining_rates) == 0:
-        my_print('shorting')
-        cached_results[key] = 0
-        return 0
-    remaining_destinations = tuple(remaining_rates.keys())
-    num_destinations = len(remaining_destinations)
-    num_agents = len(agent_current_valves)
-    if num_agents > num_destinations:
-        # We have more agents than closed valves. Some agents will take a nap (current valve = None)
-        remaining_destinations = remaining_destinations + (None,)*(num_agents - num_destinations)
-    
-    def napper_stays_napping(positions: tuple[str | None, str | None]) -> bool:
-        return positions[0] is not None or positions[1] is None
-
-    destination_permutations = get_non_repeating_product(remaining_destinations, num_agents)  # type: ignore
-    # Deduplicate
-    destination_permutations = list(set(destination_permutations))
-    subsequent_results: list[int] = [0]
-    for destinations in destination_permutations:
-        zipped_start_end = tuple(zip(agent_current_valves, destinations))
-        nappers_stay_napping = map(napper_stays_napping, zipped_start_end)  # type: ignore
-        my_print(f'{destinations=}')
-        if not all(nappers_stay_napping):
-            # Don't wake them up!
-            my_print('Dont wake em up')
-            continue
-        paths: list[list[str] | None] = list(map(path_map.get, zipped_start_end))  # type: ignore
-        my_print(f'{paths=}')
-        calculate_distance = lambda t: get_distance(t[0], t[1])
-        distances = list(map(calculate_distance, zipped_start_end))
-        my_print(f'{distances=}')
-        if type(min(distances)) == float:
-            continue
-        minimum_distance = int(min(distances))
-        time_to_activate = minimum_distance + 1
-        if remaining_time - time_to_activate <= 0:
-            continue
-        actual_destinations: list[str | None] = []
-        activated_valves: list[str] = []
-        # Step forward to next time an agent activates a valve. The other agents
-        # move as far as they can in that amount of elapsed time.
-        for path in paths:
-            if path is None:
-                actual_destinations.append(None)
-            elif len(path) == time_to_activate:
-                # Track the valves which get activated by the end of the step
-                actual_destinations.append(path[-1])
-                activated_valves.append(path[-1])
-            else:
-                actual_destinations.append(path[time_to_activate])
-        assert len(activated_valves) == len(set(activated_valves))
-        assert all(map(remaining_rates.__contains__, activated_valves))
-        pressure_released = (remaining_time - time_to_activate) * sum(map(remaining_rates.get, activated_valves))  # type: ignore
-        new_remaining = {k: v for k, v in remaining_rates.items() if k not in activated_valves}
-        if len(new_remaining) > 0 and remaining_time - time_to_activate > 1:
-            pressure_released += get_pressure_released(
-                tuple(d for d in actual_destinations if d is not None),
-                new_remaining,
-                remaining_time - time_to_activate,
-            )
-        subsequent_results.append(pressure_released)
-    result = max(subsequent_results)
-    my_print(f'{result=}')
-    cached_results[key] = result
     return result
 
 
 @time_execution
 def part_1() -> None:
-    utility = get_pressure_released(('AA',), optimizable_flows_by_label, 30)
+    utility = get_pressure_released(('AA',), tuple(optimizable_flows_by_label.keys()), 30)
     print(f'part_1={utility}')
 
 
 @time_execution
 def part_2() -> None:
-    # Populate the cache for minimal recursive calls
-    MAX_TIME = 26
-    # for max_time in tqdm(range(1, MAX_TIME)):
-    #     get_pressure_released(('AA', 'AA'), optimizable_flows_by_label, max_time)
-    utility = get_pressure_released(('AA', 'AA'), optimizable_flows_by_label, MAX_TIME)
+    utility = get_pressure_released(('AA', 'AA'), tuple(optimizable_flows_by_label.keys()), 26)
     print(f'part_2={utility}')
 
 
